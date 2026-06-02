f10ebaf169c2d11b5577a2504c93b4c5f5e51b3a
Kowshikkumar <kowshikkr16@gmail.com>
2026-06-02 17:55:13 +0530

    Create ExampleToken.sol

---
+++ ERC20/ExampleToken.sol
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/// @title ExampleToken
+/// @notice Example ERC20 token deployed via Krionex
+/// @dev Standard ERC20 with mint, burn, and pause capabilities
+
+contract ExampleToken is ERC20, ERC20Burnable, ERC20Pausable, Ownable {
+
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 initialSupply,
+        address initialOwner
+    ) ERC20(name, symbol) Ownable(initialOwner) {
+        _mint(initialOwner, initialSupply * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function _update(address from, address to, uint256 value)
+        internal
+        override(ERC20, ERC20Pausable)
+    {
+        super._update(from, to, value);
+    }
+}
